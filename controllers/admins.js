af876242372c3ce7590e838ab39b9b8ec5fabc02
Senne <>
2022-12-09 16:19:55 +0100

    fix: change nickname to username

--- controllers/admins.js
+++ controllers/admins.js
@@ -24,7 +24,7 @@ const login = async (req, res) => {
       res.json({
         status: "success",
         token: token,
-        nickname: admin.nickname,
+        username: admin.username,
       });
     } else {
       res.json({
